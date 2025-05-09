f77857112bb37894dca89d76170d15dece6e7a87
ByteDance <1677802399@qq.com>
2025-05-09 10:33:10 +0800

    实验b

--- src/pages/test1/index.js
+++ src/pages/test1/index.js
@@ -1,5 +1,4 @@
 
 import { getSongDetailArrayAction } from '../../hooks/change-state'
 import AppNavBar from '@/components/nav-bar/index'
-import theme_test1 from '@/components/theme-test1'
-const JMUser = React.lazy(() => import('@/pages/profile'))
+import theme from '@/components/theme-test'
